2b87955f87af682e7d540fc608c84c01761ce43a
mubashir <mubashir11131719@gmail.com>
2021-05-31 14:19:14 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
-const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
